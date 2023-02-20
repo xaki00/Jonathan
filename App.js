ee3bfade11f39480ac8b7c30be3e44b55f5b1b10
xaki00 <108393958+xaki00@users.noreply.github.com>
2023-02-20 13:30:26 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import LoginForm from './components/loginform';
+import './App.css'
+
+function App() {
+  return (
+    <div className="page">
+     <LoginForm />
+       
+    
+    </div>
+  );
+}
+
+
+export default App;
